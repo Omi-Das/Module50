89c6425ba98868bfb169868397b8c80e5b05281b
Omi-Das <omi06133@gmail.com>
2026-06-13 23:50:16 +0600

    Comment delete

--- src/app/users/[userid]/page.jsx
+++ src/app/users/[userid]/page.jsx
@@ -2,10 +2,8 @@
 import { getUserById } from '@/app/lib/data';
 
 const UserDetailPage = async ({ params }) => {
-    // ফোল্ডার নাম [userid] তাই এখানেও 'userid' হবে
     const { userid } = await params; 
     
-    // এবার এই 'userid' টি পাঠান
     const user = await getUserById(userid);
 
     if (!user) return <div>User not found!</div>;
